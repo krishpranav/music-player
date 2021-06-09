91a5ca669c0a4901ea5cb4dd38e48e543f740a43
krishpranav <krisna.pranav@gmail.com>
2021-06-09 19:28:27 +0530

    [+] play song [+]

---
+++ src/redux/reducers/playState.ts
@@ -0,0 +1,17 @@
+import { T_PLAY_SONG, T_PAUSE_SONG, T_RESUME_SONG } from '../types';
+
+export const playStateReducer = (
+    state = {
+        playing: false,
+        index: -1,
+    },
+    action: any
+) => {
+    switch(action.type) {
+        case T_PLAY_SONG: {
+            return { playing: true, index: action.index };
+        }
+        
+    }
+
+}
