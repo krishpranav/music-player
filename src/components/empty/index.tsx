3082fa484760a814cc511e1342f3afdd2c3a73a1
krishpranav <krisna.pranav@gmail.com>
2021-06-09 17:18:29 +0530

    [+] Empty Propts [+]

---
+++ src/components/empty/index.tsx
@@ -0,0 +1,9 @@
+import { FaExclamationTriangle } from "react-icons/fa";
+
+import './styles.css';
+
+type EmptyProps = {
+    icon?:        any;
+    message?:     string;
+    description?: string;
+};
