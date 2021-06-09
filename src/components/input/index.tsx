dd3064f1f758c1519068321a0cf10d0e0d5809ca
krishpranav <krisna.pranav@gmail.com>
2021-06-09 17:24:12 +0530

    [+] input tsx updated [+]

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -9,5 +9,31 @@ type InputProps = {
 }
 
 
+const Input = ({
+    onChange,
+    type = 'text',
+    placeholder = 'Search...',
+  }: InputProps) => {
+    const [inFocus, setFocus] = useState(false);
+    return (
+      <div
+        className={`input__wrapper ${
+          inFocus ? 'input__wrapper--focus' : ''
+        }`.trim()}
+      >
+        <input
+          type={type}
+          placeholder={placeholder}
+          onFocus={() => setFocus(true)}
+          onBlur={() => setFocus(false)}
+          onChange={e => onChange && onChange(e.target.value)}
+        />
+        <div className="input__icon">
+          <FaSearch size={24} />
+        </div>
+      </div>
+    );
+  };
+  
 
 export default Input;
