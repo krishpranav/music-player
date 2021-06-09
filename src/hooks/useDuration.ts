2f0c9db7a70bdc2841b7368963a57bbdb1d820f0
krishpranav <krisna.pranav@gmail.com>
2021-06-09 17:30:59 +0530

    [+] effect [+]

--- src/hooks/useDuration.ts
+++ src/hooks/useDuration.ts
@@ -1,7 +1,27 @@
 import { useEffect, useState } from 'react';
+import { resolve } from 'url';
 
 const useDuration = (songs: any[]) => {
     const [duration, setDuration] = useState<any>({});
 
+    useEffect(() => {
+        (async () => {
+            let data: any = {};
+            const durationPromises = songs.map(song => {
+                const audio = document.createElement('audio');
+                audio.src = URL.createObjectURL(song);
+
+                return new Promise<any>(resolve => {
+                    audio.addEventListener(
+                        'loadmetadata',
+                        function() {
+                            resolve({ [song.name]: audio.duration });
+                        },
+                        false,
+                    );
+                });
+            });
+        })
+    })
 
 }
