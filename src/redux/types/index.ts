057bb36574d3c40fcfc02f657999350e19bd7319
krishpranav <krisna.pranav@gmail.com>
2021-06-09 19:24:22 +0530

    [+] export values [+]

---
+++ src/redux/types/index.ts
@@ -0,0 +1,13 @@
+export const T_ADD_SONGS = 'ADD_SONGS';
+export const T_REMOVE_SONGS = 'REMOVE_SONGS';
+
+export const T_PLAY_SONG = 'PLAY_SONG';
+export const T_PAUSE_SONG = 'PAUSE_SONG';
+export const T_RESUME_SONG = 'RESUME_SONG';
+
+export const T_SET_GRID = 'SET_GRID';
+export const T_SET_THEME = 'SET_THEME';
+export const T_SET_REPEAT = 'SET_REPEAT';
+export const T_SET_VISUALIZER = 'SET_VISUALIZER';
+
+export const T_SET_VIEW = 'SET_VIEW';
