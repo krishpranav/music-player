c4c178ba78aa68530f2a273a3ce3adf37871db12
krishpranav <krisna.pranav@gmail.com>
2021-06-09 19:23:37 +0530

    [+] index ts [+]

--- src/redux/actions/index.ts
+++ src/redux/actions/index.ts
@@ -1,44 +1,61 @@
 import {
-  T_ADD_SONGS,
-  T_PLAY_SONG,
-  T_PAUSE_SONG,
-  T_RESUME_SONG,
-  T_REMOVE_SONGS,
-  T_SET_GRID,
-  T_SET_THEME,
-  T_SET_REPEAT,
-  T_SET_VISUALIZER,
-  T_SET_VIEW,
-} from '../types';
-
-// add songs function
-export const ADD_SONGS = (songs: any) => ({
+    T_ADD_SONGS,
+    T_PLAY_SONG,
+    T_PAUSE_SONG,
+    T_RESUME_SONG,
+    T_REMOVE_SONGS,
+    T_SET_GRID,
+    T_SET_THEME,
+    T_SET_REPEAT,
+    T_SET_VISUALIZER,
+    T_SET_VIEW,
+  } from '../types';
+  
+  export const ADD_SONGS = (songs: any) => ({
     type: T_ADD_SONGS,
     songs,
-});
-
-// delete song function
-export const DELETE_SONGS = (index: any) => ({
+  });
+  
+  export const DELETE_SONG = (index: any) => ({
     type: T_REMOVE_SONGS,
     index,
-});
-
-// const play song function
-export const PLAY_SONG = (index: number) => ({
+  });
+  
+  export const PLAY_SONG = (index: number) => ({
     type: T_PLAY_SONG,
     index,
-});
-
-// const pause song function
-export const PAUSE_SONG = () => ({
+  });
+  
+  export const PAUSE_SONG = () => ({
     type: T_PAUSE_SONG,
-});
-
-export const RESUME_SONG = () => ({
+  });
+  
+  export const RESUME_SONG = () => ({
     type: T_RESUME_SONG,
-});
-
-export const SET_GRID = (value: boolean) => ({
+  });
+  
+  export const SET_GRID = (value: boolean) => ({
+    type: T_SET_GRID,
+    value,
+  });
+  
+  export const SET_VISUALIZER = (value: boolean) => ({
     type: T_SET_VISUALIZER,
     value,
-});
+  });
+  
+  export const SET_THEME = (value: boolean) => ({
+    type: T_SET_THEME,
+    value,
+  });
+  
+  export const SET_REPEAT = (value: string) => ({
+    type: T_SET_REPEAT,
+    value,
+  });
+  
+  export const SET_VIEW = (value: string) => ({
+    type: T_SET_VIEW,
+    value,
+  });
+  
