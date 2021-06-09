b395badff27c3cdee2251d18243d50cd062c8863
krishpranav <krisna.pranav@gmail.com>
2021-06-09 17:53:16 +0530

    [+] index ts updated [+]

---
+++ src/hooks/index.ts
@@ -0,0 +1,6 @@
+import useResize from "./useResize";
+import useScroll from "./useScroll";
+import usePrevious from "./userPrevious";
+import useDuration from "./useDuration";
+
+export { useResize, useScroll, usePrevious, useDuration};
