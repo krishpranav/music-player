3cb10a6844eae591c9bbb640ff97ce10f4617541
krishpranav <krisna.pranav@gmail.com>
2021-06-10 10:41:14 +0530

    [+] declare [+]

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -3,3 +3,9 @@ import { ExpirationPlugin } from 'workbox-expiration';
 import { precacheAndRoute, createHandlerBoundToURL } from 'workbox-precaching';
 import { registerRoute } from 'workbox-routing';
 import { StaleWhileRevalidate } from 'workbox-strategies';
+
+declare const self: ServiceWorkerGlobalScope;
+
+clientsClaim();
+
+precacheAndRoute(self.__WB_MANIFEST);
