bdfc458314f315ef4c8acf0082e76a50323b8169
krishpranav <krisna.pranav@gmail.com>
2021-06-10 10:40:31 +0530

    [+] imports [+]

---
+++ src/service-worker.ts
@@ -0,0 +1,5 @@
+import { clientsClaim } from 'workbox-core';
+import { ExpirationPlugin } from 'workbox-expiration';
+import { precacheAndRoute, createHandlerBoundToURL } from 'workbox-precaching';
+import { registerRoute } from 'workbox-routing';
+import { StaleWhileRevalidate } from 'workbox-strategies';
