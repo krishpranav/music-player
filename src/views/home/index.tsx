a28fdc478b2fb28240ddf43f243d0bd696fde4ba
krishpranav <krisna.pranav@gmail.com>
2021-06-10 08:30:32 +0530

    [+] home page [+]

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -2,8 +2,26 @@ import Input from '../../components/input';
 import './styles.css';
 
 type HomeProps = {
-    onSearch?: Function,
-    showSearch: boolean;
-    playlist: React.ReactNode;
+  onSearch?: Function;
+  showSearch: boolean;
+  playlist: React.ReactNode;
 };
 
+const Home = ({ onSearch, showSearch, playlist }: HomeProps) => {
+  return (
+    <div className="home">
+      {showSearch && (
+        <div className="home__input">
+          <Input
+            type="search"
+            placeholder="Search"
+            onChange={(e: string) => onSearch && onSearch(e)}
+          />
+        </div>
+      )}
+      {playlist}
+    </div>
+  );
+};
+
+export default Home;
