4f107a01070f16ac6ecd3e9d805ea5a8c75524f2
krishpranav <krisna.pranav@gmail.com>
2021-06-10 10:37:37 +0530

    [+] index [+]

--- src/index.tsx
+++ src/index.tsx
@@ -13,3 +13,41 @@ import { reducers } from './redux';
 import DataStore from './services/data-store';
 
 const WINDOW = window as any;
+
+(async () => {
+  try {
+    let localStore: any = await DataStore.get('state');
+
+    if (localStore === null) {
+      localStore = undefined;
+    }
+
+    const store = createStore(
+      reducers,
+      localStore,
+      WINDOW.__REDUX_DEVTOOLS_EXTENSION__ &&
+        WINDOW.__REDUX_DEVTOOLS_EXTENSION__(),
+    );
+
+    store.subscribe(() => {
+      DataStore.set('state', {
+        songs: store.getState().songs,
+        settings: store.getState().settings,
+      });
+    });
+
+    ReactDOM.render(
+      <React.StrictMode>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </React.StrictMode>,
+      document.getElementById('root'),
+    );
+  } catch (error) {}
+})();
+
+
+serviceWorkerRegistration.register();
+
+reportWebVitals();
