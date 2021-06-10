da5a97f9c9982d453a6655a371a5109d1bee741f
krishpranav <krisna.pranav@gmail.com>
2021-06-10 08:12:23 +0530

    [+] static get function [+]

--- src/services/data-store.ts
+++ src/services/data-store.ts
@@ -9,4 +9,8 @@ export default class DataStore {
     public static set = (key: string, value: any, callback?: any) => {
         return localforage.setItem(key, value, callback);
     };
+
+    public static get = (key: string, callback?: any) => {
+        return localforage.getItem(key, callback);
+    };
 }
